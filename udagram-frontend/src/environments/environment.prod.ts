a819e46775913f4cce5270d96daf11c9fa6d367a
olaniyi-ogungbe <olaniyiogungbe@gmail.com>
2022-10-26 01:33:27 +0100

    added public reverseproxy to frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9d1b5fb314544204b6cf17208bb92ba-2030005425.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
